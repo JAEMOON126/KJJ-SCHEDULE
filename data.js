b5fe428704653278a3371f6f96c6c61266f2eaae
litalu0811-creator <litalu0811-creator@users.noreply.github.com>
2026-07-27 12:06:09 +0800

    更新日程与动态 2026-07-27 12:06

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.YT_DATA = {
-  "checkedAt": "2026-07-26T10:06:42.902Z",
+  "checkedAt": "2026-07-27T04:06:09.111Z",
   "monitored": [
     {
       "id": "jj_weibo",
@@ -16,8 +16,8 @@ window.YT_DATA = {
       "url": "https://weibo.com/u/7830607498",
       "status": "ok",
       "hasNew": true,
-      "latest": "金在中 ins直播中！！！",
-      "latestTime": "10分钟前"
+      "latest": "金在中 💚 260727 #金在中# 韩网新闻： 【BS议题】「顶级明星」不安于现状，音乐节目夺冠的「制作人」、金在中",
+      "latestTime": "25分钟前"
     },
     {
       "id": "yinfu_weibo",
@@ -26,16 +26,16 @@ window.YT_DATA = {
       "status": "ok",
       "hasNew": false,
       "latest": "金在中 宣传视频来啦 @iNKODEofficial: 📢[#公告#][#Notice#] 2026 KIM JAE ",
-      "latestTime": "6小时前"
+      "latestTime": "7-26 11:36"
     },
     {
       "id": "inkode_weibo",
       "name": "iNKODE · 官方微博",
       "url": "https://weibo.com/u/7836781664",
       "status": "ok",
-      "hasNew": false,
-      "latest": "📢[#公告#][#Notice#] 2026 KIM JAE JOONG CONCERT [THE WAVE] in ",
-      "latestTime": "7小时前"
+      "hasNew": true,
+      "latest": "📢[#公告#][#Notice#] KIM JAE JOONG OFFICIAL FANCLUB ‘BOSS BABI",
+      "latestTime": "1小时前"
     },
     {
       "id": "jj_chaohua",
@@ -54,25 +54,24 @@ window.YT_DATA = {
       "status": "ok",
       "hasNew": true,
       "latest": "00:34 【金在中】2026 KIM JAE JOONG CONCERT [THE WAVE] in Seoul SP",
-      "latestTime": "7小时前 · 投稿了视频"
+      "latestTime": "昨天 11:00 · 投稿了视频"
     },
     {
       "id": "dreamyroad_bili",
       "name": "梦露 DreamyRoad · B站",
       "url": "https://space.bilibili.com/3493284984916609",
-      "status": "pending",
-      "note": "打开页面出错：page.goto: Timeout 40000ms exceeded.\nCall log:\n  - navigating to \"https://space.bilibili.com/3493284984916609/dynamic\", waiting until \"networkidle\"\n",
+      "status": "ok",
       "hasNew": false,
-      "latest": "打开页面出错：page.goto: Timeout 40000ms exceeded.\nCall log:\n  - navigating to \"https://space.bilibili.com/3493284984916609/dynamic\", waiting until \"networkidle\"\n",
-      "latestTime": null
+      "latest": "💚梦路招新💚 欢迎大家加入我们哟～！！！",
+      "latestTime": "2025年12月28日"
     },
     {
       "id": "jj_instagram",
       "name": "金在中 · Instagram",
       "url": "https://www.instagram.com/jj_1986_jj",
       "status": "ok",
-      "hasNew": false,
-      "latest": "最新帖 DbHkv0VEyZr",
+      "hasNew": true,
+      "latest": "最新帖 DbQrJNEIAmV",
       "latestTime": null
     },
     {
@@ -90,8 +89,8 @@ window.YT_DATA = {
       "name": "金在中 · 日本官咖",
       "url": "https://jaefans.com/",
       "status": "ok",
-      "hasNew": false,
-      "latest": "2026.07.24TOPICS ねこり公式サイトにてムービーが公開！",
+      "hasNew": true,
+      "latest": "2026.07.27RELEASE 日本デジタルシングル『道標』本日7月27日(月)デジタル配信スタート！",
       "latestTime": null
     }
   ]
