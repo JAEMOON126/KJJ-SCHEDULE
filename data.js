a799da809722163ab11a8a8f7106eddd6c40bb6c
litalu0811-creator <litalu0811-creator@users.noreply.github.com>
2026-09-10 12:06:08 +0800

    更新日程与动态 2026-09-10 12:06

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.YT_DATA = {
-  "checkedAt": "2026-09-09T10:06:13.385Z",
+  "checkedAt": "2026-09-10T04:06:08.116Z",
   "monitored": [
     {
       "id": "jj_weibo",
@@ -17,9 +17,9 @@ window.YT_DATA = {
       "url": "https://weibo.com/u/7830607498",
       "status": "ok",
       "hasNew": true,
-      "latest": "金在中 💚 260909 #金在中# ins story② @金在中_Official @JJ860126JJ",
-      "latestTime": "1小时前",
-      "updatedAt": "2026-09-09T10:05:31.421Z"
+      "latest": "金在中//@金在中_Official:在成都见面吧！ @星羽娱乐: 2026 KIM JAE JOONG FANMEET",
+      "latestTime": "9-9 11:15",
+      "updatedAt": "2026-09-10T04:05:31.243Z"
     },
     {
       "id": "yinfu_weibo",
@@ -37,9 +37,9 @@ window.YT_DATA = {
       "url": "https://weibo.com/u/7836781664",
       "status": "ok",
       "hasNew": true,
-      "latest": "🎞[#在tube#][#Jaetube#] JAEJOONG 'OASIS / 道標' Recording Behin",
-      "latestTime": "5分钟前",
-      "updatedAt": "2026-09-09T10:05:42.318Z"
+      "latest": "转发微博 @星羽娱乐: 2026 KIM JAE JOONG FANMEETING 「只蜀于你」 来自在中的问候视频 \ud83d",
+      "latestTime": "56分钟前",
+      "updatedAt": "2026-09-10T04:05:42.231Z"
     },
     {
       "id": "jj_chaohua",
@@ -57,29 +57,29 @@ window.YT_DATA = {
       "url": "https://space.bilibili.com/693071922",
       "status": "ok",
       "hasNew": true,
-      "latest": "09:10 【金在中】JAEJOONG 'OASIS / 道標' Recording Behind #金在中 #KIMJ",
-      "latestTime": "5分钟前 · 投稿了视频",
-      "updatedAt": "2026-09-09T10:05:47.594Z"
+      "latest": "00:39 【金在中】只有前1%的人才能找到 🦎🎨 #金在中 #KIMJAEJOONG Copyrights 202",
+      "latestTime": "15小时前 · 投稿了视频",
+      "updatedAt": "2026-09-10T04:05:46.865Z"
     },
     {
       "id": "dreamyroad_bili",
       "name": "梦露 DreamyRoad · B站",
       "url": "https://space.bilibili.com/3493284984916609",
       "status": "ok",
-      "hasNew": true,
+      "hasNew": false,
       "latest": "花絮来啦！～ 查看图片 金在中_Official 投稿了视频 09:10 【金在中】JAEJOONG 'OASIS / ",
-      "latestTime": "2分钟前",
+      "latestTime": "18小时前",
       "updatedAt": "2026-09-09T10:05:52.023Z"
     },
     {
       "id": "jj_instagram",
       "name": "金在中 · Instagram",
       "url": "https://www.instagram.com/jj_1986_jj",
-      "status": "pending",
-      "note": "没读到帖子（可能被风控）",
+      "status": "ok",
       "hasNew": false,
-      "latest": "没读到帖子（可能被风控）",
-      "latestTime": null
+      "latest": "最新帖 Dc_EsXvE7l3",
+      "latestTime": null,
+      "updatedAt": "2026-09-07T12:36:44.516Z"
     },
     {
       "id": "jj_weverse",
@@ -96,7 +96,7 @@ window.YT_DATA = {
       "name": "金在中 · 日本官咖",
       "url": "https://jaefans.com/",
       "status": "ok",
-      "hasNew": true,
+      "hasNew": false,
       "latest": "2026.09.09RELEASE 日本デジタルシングル『OASIS / 道標』レコーディングビハインド映像が公開！",
       "latestTime": null,
       "updatedAt": "2026-09-09T10:06:07.587Z"
